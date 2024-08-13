5ac023fb473b6f687bccadecae67d665e386992d
amasson <arthur.masson.3896@gmail.com>
2024-08-13 16:12:45 +0200

    FactionData target attitude

--- Source/FactionSystem/Private/FactionSystem/FactionData.cpp
+++ Source/FactionSystem/Private/FactionSystem/FactionData.cpp
@@ -3,3 +3,57 @@
 
 #include "FactionSystem/FactionData.h"
 
+static uint8 _TeamAttitudes[256][256];
+static bool _TeamAttitudesInitialized = false;
+
+static void _InitializeTeamAttitudes()
+{
+    if (_TeamAttitudesInitialized)
+        return;
+
+    for (int i = 0; i < 256; ++i)
+    {
+        for (int j = 0; j < 256; ++j)
+        {
+            _TeamAttitudes[i][j] = static_cast<uint8>(ETeamAttitude::Neutral);
+        }
+    }
+    _TeamAttitudesInitialized = true;
+}
+
+ETeamAttitude::Type FactionSystemAttitudeSolver(FGenericTeamId TeamId, FGenericTeamId OtherTeamId)
+{
+    return static_cast<ETeamAttitude::Type>(_TeamAttitudes[TeamId.GetId()][OtherTeamId.GetId()]);
+}
+
+UFactionData::UFactionData()
+{
+
+}
+
+void UFactionData::PostLoad()
+{
+    Super::PostLoad();
+
+    _InitializeTeamAttitudes();
+    for (int i = 0; i < 256; ++i)
+    {
+        _TeamAttitudes[TeamId.GetId()][i] = static_cast<uint8>(DefaultAttitude);
+    }
+    for (auto& Attitude : Attitudes)
+    {
+        if (!IsValid(Attitude.Key))
+            continue;
+
+        _TeamAttitudes[TeamId.GetId()][Attitude.Key->TeamId.GetId()] = static_cast<uint8>(Attitude.Value);
+    }
+    FGenericTeamId::SetAttitudeSolver(FactionSystemAttitudeSolver);
+}
+
+void UFactionData::SetAttitudeTowardFaction(UFactionData* OtherFaction, TEnumAsByte<ETeamAttitude::Type> Attitude)
+{
+    if (!IsValid(OtherFaction))
+        return;
+
+    _TeamAttitudes[TeamId.GetId()][OtherFaction->TeamId.GetId()] = static_cast<uint8>(Attitude);
+}
--- Source/FactionSystem/Public/FactionSystem/FactionData.h
+++ Source/FactionSystem/Public/FactionSystem/FactionData.h
@@ -18,17 +18,23 @@ class FACTIONSYSTEM_API UFactionData : public UDataAsset
 
 public:
 
-    /** Team ID */
+    UFactionData();
+
+    void PostLoad() override;
+
+    /** Team ID. Pick a different one for each Faction Data */
     UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Team")
     FGenericTeamId TeamId;
 
-    // TODO: Create relation system and use custom attitude solver
-    // /** Attitude against others factions */
-    // UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Attitude")
-    // TMap<UFactionData*, TEnumAsByte<ETeamAttitude::Type>> Attitudes;
+    /** Attitude against others factions */
+    UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Attitude")
+    TMap<UFactionData*, TEnumAsByte<ETeamAttitude::Type>> Attitudes;
 
-    // /** Attitude against unknown factions */
-    // UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Attitude")
-    // TEnumAsByte<ETeamAttitude::Type> DefaultAttitude = ETeamAttitude::Neutral;
+    /** Attitude against unknown factions */
+    UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Attitude")
+    TEnumAsByte<ETeamAttitude::Type> DefaultAttitude = ETeamAttitude::Neutral;
+
+    UFUNCTION(BlueprintCallable, Category = "Attitude")
+    void SetAttitudeTowardFaction(UFactionData* OtherFaction, TEnumAsByte<ETeamAttitude::Type> Attitude);
 
 };
